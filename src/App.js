73f0eebecb7a2d5674f1e13652d7d1fa1b42bc27
James <mxtery@gmail.com>
2019-12-15 00:32:18 -0600

    added bird animation

--- src/App.js
+++ src/App.js
@@ -23,6 +23,18 @@ class App extends Component{
 
         return (
             <div>
+                <div class="bird-container bird-container--one">
+                <div class="bird bird--one"></div>
+                </div>
+                <div class="bird-container bird-container--two">
+                <div class="bird bird--two"></div>
+                </div>
+                <div class="bird-container bird-container--three">
+                <div class="bird bird--three"></div>
+                </div>
+                <div class="bird-container bird-container--four">
+                <div class="bird bird--four"></div>
+                </div>
                 <img src={profile} className="profile" alt="profile"/>
                 <h1>Hello!</h1>
                 <p>My name is James Eroy</p>
