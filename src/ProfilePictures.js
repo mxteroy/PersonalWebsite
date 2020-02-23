d4cad353c2568290b7f47c24a9dace35cc50da4a
James <mxteroy@gmail.com>
2020-02-22 20:45:33 -0600

    added picture

--- src/ProfilePictures.js
+++ src/ProfilePictures.js
@@ -4,7 +4,7 @@ import profile1 from './assets/profile1.jpg';
 import profile2 from './assets/profile2.jpg';
 import profile3 from './assets/profile3.jpg';
 import profile4 from './assets/profile4.jpg';
-import profile6 from './assets/profile4.jpg';
+import profile6 from './assets/profile6.jpg';
 
 const PROFILEPICTURES = [
     profile6
@@ -38,7 +38,7 @@ class ProfilePictures extends Component {
 
         return (
             // <img className={(fadeIn ? 'fade-in' : 'fade-out') + " profile"} src={profilePicture} alt="profile"/>
-            <img src={profilePicture} alt="profile" />
+            <img className="profile" src={profilePicture} alt="profile" />
         )
     }
 }
