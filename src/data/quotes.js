1766e9b90437c21c78dab7ba27af4b1a6a1d568e
James <mxteroy@gmail.com>
2020-04-02 02:24:12 -0500

    changed quote

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,5 +1,5 @@
 const QUOTES = [
-    'Find that passion and let it kill you. Let it take over your life.',
+    'Find that PURPOSE and let it kill you. Let it take over your life.',
     'People are most beautiful when they\'re confident. Smile!',
     'We all make mistakes, it\'s what we do about those mistakes that define us!'
 ];
