10d04d4babcd6b2d359ccdbe1f552ec12dd4132a
mxteroy <mxtery@gmail.com>
2020-09-27 01:57:00 -0500

    updated descriptions

--- src/data/experienceDescs.js
+++ src/data/experienceDescs.js
@@ -5,20 +5,20 @@ const FACEBOOK = [
 ]
 
 const PANW = [
-    <p>Implemented log generation for an internal Cortex Data Lake testing tool that reduces test times by 100%</p>,
-    <p>Created Python script that parses schema files for automatic generation of C header files</p>,
-    <p>Redesigned and developed modern UI in for an internal Cortex Data Lake diagnostic tool</p>
+    <p>Implemented mock log generation with C and Docker on internal firewall simulator to improve test times by 100%</p>,
+    <p>Automated the creation of C header files through developing a Python script that parses log schematic files </p>,
+    <p>Designed and developed a Flask architectural diagram app of Cortex Data Lake to diagnose its layer/segments</p>
 ]
 
 const CHARLESSCHWAB = [
-    <p>Led in the development a Risk-Based Authentication feature to verify a user attempting to change their password</p>,
-    <p>Developed application feature, in an <strong>Agile Scrum</strong> environment, for changing the password of a Schwab account</p>,
-    <p>Utilized full-stack knowledge through use of <strong>Angular 6</strong>, <strong>Jasmine</strong>, <strong>Java</strong>, <strong>MongoDB</strong> and unit testing in <strong>Karma</strong></p>
+    <p>Developed the “change your password” webpage for the Charles Schwab website using Angular</p>,
+    <p>Led and collaborated the development of “risk-based authentication” webpage that verifies the identity of users</p>,
+    <p>Utilized full-stack knowledge through use of Angular, Jasmine, MongoDB, and Karma unit testing</p>
 ]
 
 const ANTHROPOLOGY = [
-    <p>Developed a <strong>full-stack web application</strong> using the <strong>Flask</strong> framework that connects anthropologists around the world</p>,
-    <p>Implemented a forum for shipwreck discussion and scraped big data for manipulation and displayed on a UI</p>
+    <p>Created a full-stack platform that acts as a forum to connect anthropologists around the world </p>,
+    <p>Coordinated with a multi-disciplinary group of professors and students to delegate the library’s projects</p>
 ]
 
 
