d87545f57551528bc9627e498480fb52f56b5850
mxteroy <mxtery@gmail.com>
2020-09-27 02:03:20 -0500

    updated

--- src/data/projectDescs.js
+++ src/data/projectDescs.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
 const FIREAULDESCRIPTIONS = [
-<p>Developed front-end using <strong>ReactJS</strong> that is hosted using Firebase while utilizing the <strong>Realtime Database</strong></p>,
-<p>Built <strong>back-end API</strong> with a <strong>Flask server</strong> (to query and <strong>asynchronously listen</strong> for data entries and delivering emergency alerts through the <strong>Twilio API</strong>) hosted on an <strong>AWS EC2 instance</strong></p>,
-<p>Integrated <strong>Google Cloud Platform’s Cloud Vision API</strong> for categorization and image classification and <strong>Maps API</strong> for marking fires</p>
+<p>Led the development of a full-stack application to allow people to monitor and receive help during wildfires</p>,
+<p>Integrated an interactive Google map and Google Vision API image parsing for object classification</p>,
+<p>Deployed front-end using React and REST API with Flask on AWS EC2 instance</p>
 ]
 
 const AGGIESECTIONDESCRIPTIONS = [
-    <p>Developed <strong>back-end web crawler </strong>using <strong>Selenium</strong> to traverse the school’s course information website</p>,
-    <p>Developed front-end using <strong>ReactJS</strong> that is hosted using Firebase while utilizing the <strong>Realtime Database</strong></p>,
-    <p>Effectively <strong>scanned hundreds</strong> of course sections and, overall, notified <strong>72</strong> students for open sections using the <strong>Twilio API</strong></p>
+    <p>Developed application to notify students when a course section is open using Python, Selenium, and Firebase</p>,
+    <p>Used by 70+ students at its peak but had to cease operation after 16 hours due to conflict with university rules</p>
 ]
 
 const BESTDARNTACODESCRIPTIONS = [
