4e6de24092d514570a5c0e5346a519483121527e
mxteroy <mxteroy@gmail.com>
2021-01-09 18:30:02 -0600

    disabled quote and enabled profile picture

--- src/App.js
+++ src/App.js
@@ -1,68 +1,63 @@
-import React, { Component } from 'react';
-import SocialProfiles from './Socials';
-import Title from './Title';
-import MyNavbar from './MyNavbar.js';
-import ProfilePictures from './ProfilePictures.js';
-import BirdsAndClouds from './BirdsAndClouds.js';
-import Sections from './Sections.js';
-import TypingText from './TypingText.js';
+import React, { Component } from "react";
+import SocialProfiles from "./Socials";
+import Title from "./Title";
+import MyNavbar from "./MyNavbar.js";
+import ProfilePictures from "./ProfilePictures.js";
+import BirdsAndClouds from "./BirdsAndClouds.js";
+import Sections from "./Sections.js";
+import TypingText from "./TypingText.js";
 
-import PROJECTS from './data/projects';
-import EXPERIENCES from './data/experiences';
-import QUOTES from './data/quotes.js';
+import PROJECTS from "./data/projects";
+import EXPERIENCES from "./data/experiences";
+import QUOTES from "./data/quotes.js";
 
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { displayBio: false };
+    console.log("component this", this);
 
-class App extends Component{
+    this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+  }
 
-    constructor() {
-        super();
-        this.state = { displayBio: false };
-        console.log('component this', this);
+  toggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio });
+  };
 
-        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
-    }
-
-    toggleDisplayBio = () => {
-        this.setState({ displayBio: !this.state.displayBio });
-    }
-
-    render() {
-
-        return (
-            <div className="container">
-                <BirdsAndClouds/>
-                <MyNavbar/>
-                <Router>
-                    <Switch>
-                        <Route path="/resume">
-                            <Sections section="experiences" contents={EXPERIENCES}/>
-                            <Sections section="recent projects" contents={PROJECTS}/>
-                        </Route>
-                        <Route path="/notes">
-                            <p> Notes</p>
-                        </Route>
-                        <Route path="/">
-                            {/* <ProfilePictures/> */}
-                            <hr />
-                            <strong><TypingText texts={QUOTES} /></strong>
-                            <hr />
-                            <p>Howdy! My name is James Maxell Eroy</p>
-                            <p>Computer Science, Math, and Economics at University of Texas at Dallas</p>
-                            <hr />
-                        </Route>
-                    </Switch>
-                </Router>
-                <SocialProfiles />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <BirdsAndClouds />
+        <MyNavbar />
+        <Router>
+          <Switch>
+            <Route path="/resume">
+              <Sections section="experiences" contents={EXPERIENCES} />
+              <Sections section="recent projects" contents={PROJECTS} />
+            </Route>
+            <Route path="/notes">
+              <p> Notes</p>
+            </Route>
+            <Route path="/">
+              <ProfilePictures />
+              <hr />
+              {/* <strong><TypingText texts={QUOTES} /></strong>
+              <hr /> */}
+              <p>Howdy! My name is James Maxell Eroy</p>
+              <p>
+                Computer Science, Math, and Economics at University of Texas at
+                Dallas
+              </p>
+              <hr />
+            </Route>
+          </Switch>
+        </Router>
+        <SocialProfiles />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/ProfilePictures.js
+++ src/ProfilePictures.js
@@ -1,46 +1,49 @@
-import React, { Component } from 'react';
-import profile from './assets/profile.jpg';
-import profile1 from './assets/profile1.jpg';
-import profile2 from './assets/profile2.jpg';
-import profile3 from './assets/profile3.jpg';
-import profile4 from './assets/profile4.jpg';
-import profile6 from './assets/profile6.jpg';
-import bitmoji from './assets/bitmojicomp.JPG';
-const PROFILEPICTURES = [
-    bitmoji
-];
+import React, { Component } from "react";
+import profile from "./assets/profile.jpg";
+import profile1 from "./assets/profile1.jpg";
+import profile2 from "./assets/profile2.jpg";
+import profile3 from "./assets/profile3.jpg";
+import profile4 from "./assets/profile4.jpg";
+import profile6 from "./assets/profile6.jpg";
+import bitmoji from "./assets/bitmojicomp.JPG";
+const PROFILEPICTURES = [bitmoji];
 
 class ProfilePictures extends Component {
-    state = { pictureIndex: 0, fadeIn: true};
+  state = { pictureIndex: 0, fadeIn: true };
 
-    componentDidMount () {
-        this.timeout = setTimeout(() => this.setState({ fadeIn: false}), 4000);
-        this.animateTitles();
-    }
+  componentDidMount() {
+    this.timeout = setTimeout(() => this.setState({ fadeIn: false }), 4000);
+    this.animateTitles();
+  }
 
-    componentWillUnmount () {
-        clearInterval(this.titleInterval);
-        clearTimeout(this.timeout);
-    }
+  componentWillUnmount() {
+    clearInterval(this.titleInterval);
+    clearTimeout(this.timeout);
+  }
 
-    animateTitles = () => {
-        this.titleInterval = setInterval(() => {
-            const pictureIndex = (this.state.pictureIndex + 1) % PROFILEPICTURES.length;
+  animateTitles = () => {
+    this.titleInterval = setInterval(() => {
+      const pictureIndex =
+        (this.state.pictureIndex + 1) % PROFILEPICTURES.length;
 
-            this.setState({ pictureIndex,  fadeIn: true });
-            setTimeout(() => this.setState({ fadeIn: false}), 4000);
-        }, 8000);
-    }
+      this.setState({ pictureIndex, fadeIn: true });
+      setTimeout(() => this.setState({ fadeIn: false }), 4000);
+    }, 8000);
+  };
 
-    render() {
-        const {fadeIn, pictureIndex} = this.state;
-        const profilePicture = PROFILEPICTURES[pictureIndex];
+  render() {
+    const { fadeIn, pictureIndex } = this.state;
+    const profilePicture = PROFILEPICTURES[pictureIndex];
 
-        return (
-            // <img className={(fadeIn ? 'fade-in' : 'fade-out') + " profile"} src={profilePicture} alt="profile"/>
-            <img className="profile" src={profilePicture} alt="profile" />
-        )
-    }
+    return (
+      //   <img
+      //     className={(fadeIn ? "fade-in" : "fade-out") + " profile"}
+      //     src={profilePicture}
+      //     alt="profile"
+      //   />
+      <img className="profile" src={profile6} alt="profile" />
+    );
+  }
 }
 
 export default ProfilePictures;
